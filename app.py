4ca668a0edad3485344ae07b99a3b2aa9d5e04cf
joly54 <danyla958@gmail.com>
2023-07-25 14:31:57 +0300

    updates

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import os.path as op
 
@@ -6,24 +7,31 @@ from flask_admin import Admin
 from flask_admin.contrib.fileadmin import FileAdmin
 from flask_admin.contrib.sqla import ModelView
 from flask_cors import CORS
+from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from flask_babel import Babel
 from flasgger import Swagger
-
 
 app = Flask(__name__)
 swagger = Swagger(app)
 CORS(app, supports_credentials=True)
 api = Api(app)
+babel = Babel(app)
+app.extensions['babel'] = babel
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///base.db"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secretkey"
 db = SQLAlchemy(app)
-admin = Admin(app, template_mode='bootstrap4', name='Vin-cinema')
+migrate = Migrate(app, db)
+admin = Admin(app, template_mode='bootstrap4')
 path = op.join(op.dirname(__file__), 'static')
 admin.add_view(FileAdmin(path, '/static/', name='File Management'))
 base_url = "http://127.0.0.1:5000/"
+# make fluid layout
+app.config['FLASK_ADMIN_FLUID_LAYOUT'] = True
 
 
 class Product(db.Model):
@@ -31,27 +39,56 @@ class Product(db.Model):
     name = db.Column(db.String(80))
     price = db.Column(db.Integer)
     description = db.Column(db.String(200))
-    image = db.Column(db.String(200))
+    # image = db.Column(db.String(200))
     category = db.Column(db.String(200))
 
     def __repr__(self):
         return '<Product %r>' % self.name
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(200), ForeignKey('product.name'))
+    image = db.Column(db.String(200))
+
+    product = db.relationship('Product', backref='images', lazy=True)
+
+
+class imageViews(ModelView):
+    column_display_pk = True
+    column_hide_backrefs = False
+    column_list = ['product_name', 'image']
+    # add field for form view
+    form_columns = ['image', 'product']
+
+
 admin.add_view(ModelView(Product, db.session))
+admin.add_view(imageViews(Image, db.session))
 
 
 @app.route('/files/<path:path>')
 def send_static_files(path):
     return send_from_directory('', path)
 
-class FillDB(Resource):
-    def get(self):
-        imgs = os.listdir('static')
-        for img in imgs:
-            new_product = Product(name=img, price=100, description='description', image=base_url + "static/" + img,
-                                  category='category')
-            db.session.add(new_product)
-            db.session.commit()
-        return {'result': 'success'}
+
+@app.route('/filldb')
+def filldb():
+    import requests as rq
+    url = "https://nothingtosay322.pythonanywhere.com/products?page=1&size=99999999"
+    r = rq.get(url)
+    data = r.json()
+    for product in data['products']:
+        new_product = Product(
+            name=product['name'],
+            price=product['price'],
+            description=product['description'],
+            category=product['category'])
+        db.session.add(new_product)
+        db.session.commit()
+        new_image = Image(image=product['image'], product=new_product)
+        db.session.add(new_image)
+        db.session.commit()
+    return {'result': 'success'}
 
 
 class Products(Resource):
@@ -78,16 +115,21 @@ class Products(Resource):
         products = Product.query.paginate(page=page, per_page=size).items
         serialized_products = []
         for product in products:
+            images = Image.query.filter_by(product_name=product.name).all()
+            images = [image.image for image in images]
             serialized_product = {
                 'id': product.id,
                 'name': product.name,
                 'price': product.price,
                 "description": product.description,
-                "image": product.image,
+                "images": images,
+                "category": product.category,
             }
             serialized_products.append(serialized_product)
 
-        return {'products': serialized_products}
+        return {
+            "Total": Product.query.count(),
+            'products': serialized_products}
 
 
 class GetProduct(Resource):
@@ -112,18 +154,19 @@ class GetProduct(Resource):
         if product is None:
             return {'product': None}, 404
 
+        images = Image.query.filter_by(product_name=product.name).all()
+        images = [image.image for image in images]
         serialized_product = {
             'id': product.id,
             'name': product.name,
             'price': product.price,
             "description": product.description,
-            "image": product.image,
+            "images": images,
+            "category": product.category,
         }
-
         return {'product': serialized_product}
 
 
-api.add_resource(FillDB, '/filldb')
 api.add_resource(Products, '/products')
 api.add_resource(GetProduct, '/product/<int:id>')
 
